b621caeee3df5e8e824f8897c8f0dac3caab6ab1
许鸿志 <847118663@qq.com>
2024-01-17 13:48:45 +0800

    添加设备认证

---
+++ src/main/java/cn/piao888/user/security/config/device/DeviceClientAuthenticationToken.java
@@ -0,0 +1,27 @@
+package cn.piao888.user.security.config.device;
+
+import com.alibaba.nacos.shaded.org.checkerframework.checker.nullness.qual.Nullable;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+
+import java.util.Map;
+
+/**
+ * @Author： hongzhi.xu
+ * @Date: 2024/1/17 11:00
+ * @Version 1.0
+ */
+public class DeviceClientAuthenticationToken extends OAuth2ClientAuthenticationToken {
+
+    public DeviceClientAuthenticationToken(String clientId, ClientAuthenticationMethod clientAuthenticationMethod,
+                                           @Nullable Object credentials, @Nullable Map<String, Object> additionalParameters) {
+        super(clientId, clientAuthenticationMethod, credentials, additionalParameters);
+    }
+
+    public DeviceClientAuthenticationToken(RegisteredClient registeredClient, ClientAuthenticationMethod clientAuthenticationMethod,
+                                           @Nullable Object credentials) {
+        super(registeredClient, clientAuthenticationMethod, credentials);
+    }
+
+}
