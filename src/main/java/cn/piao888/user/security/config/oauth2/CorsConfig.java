5f996009e033e9b62deb31fac9ef4c8353199a23
许鸿志 <847118663@qq.com>
2024-09-03 11:37:44 +0800

    解决依赖问题

--- src/main/java/cn/piao888/user/security/config/oauth2/CorsConfig.java
+++ src/main/java/cn/piao888/user/security/config/oauth2/CorsConfig.java
@@ -3,6 +3,7 @@ package cn.piao888.user.security.config.oauth2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.DefaultCorsProcessor;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
@@ -16,7 +17,7 @@ public class CorsConfig {
         // 设置访问源地址
         config.addAllowedOriginPattern("*");
 //        config.addAllowedOrigin("*");
-        config.addAllowedOrigin("http://login.gonkamasn.com/");
+        config.addAllowedOrigin("*");
         // 设置访问源请求头
         config.addAllowedHeader("*");
         // 设置访问源请求方法
